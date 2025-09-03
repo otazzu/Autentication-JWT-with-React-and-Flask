6cbd035a5ef46c0358d8ae7d6931c911e10ff493
Darío Otazú <srotazu@gmail.com>
2025-09-03 13:08:08 +0000

    Updated loading UIX

--- src/front/pages/Welcome.jsx
+++ src/front/pages/Welcome.jsx
@@ -10,52 +10,55 @@ export const Welcome = () => {
     const navigate = useNavigate()
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                setEnable(true)
-                const response = await userService.ProtectedPage()
-                if (!response.success) {
-                    setEnable(false)
-                    setError(response.error)
-                    setTimeout(() => {
-                        navigate("/login")
-                    }, 3000);
-
-                }
-            } catch (error) {
-
-            }
-
-        }
         fetchData()
-    }, [])
+    }, [navigate])
+
+    const fetchData = async () => {
+        try {
+            setEnable(true)
+            const response = await userService.ProtectedPage()
+            if (!response.success) {
+                setEnable(false)
+                setError(response.error)
+                countDown()
+            }
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    const countDown = () => {
+        setTimeout(() => {
+            navigate("/login")
+        }, 3000)
+    }
 
     return (
         <div>
-            {enable ? <div className="text-center mt-5">
-                <h1 className="mb-5">Welcome!</h1>
-                <Link to="/login" className="btn btn-primary w-50">Log out</Link>
-            </div>
+            {enable ?
+                <div className="text-center mt-5">
+                    <h1 className="mb-5">Welcome!</h1>
+                    <Link to="/login" className="btn btn-primary w-50">Log out</Link>
+                </div>
                 :
                 (
-                    <div className="d-flex vh-100">
-                        <div className="alert alert-danger w-100 m-auto" role="alert">
-                            <div className="d-flex justify-content-center">
-                                {error}
+                    <div className="d-flex flex-column vh-100">
+                        <div className="my-auto">
+                            <div className="alert alert-danger w-100" role="alert">
+                                <div className="d-flex justify-content-center">
+                                    {error}
+                                </div>
+                            </div>
+                            <div className="d-flex justify-content-center align-items-center">
+                                <p className="my-0 mx-2">Redirigiendo al login</p>
+                                <div className="spinner-border spinner-border-sm" role="status">
+                                    <span className="visually-hidden">Loading...</span>
+                                </div>
                             </div>
                         </div>
                     </div>
                 )}
         </div>
-
-        // <div className="text-center mt-5">
-        //     <h1 className="mb-5">Welcome!</h1>
-        //     {error && (
-        //         <div className="alert alert-danger" role="alert">
-        //             {error}
-        //         </div>
-        //     )}
-        //     <Link to="/login" className="btn btn-primary w-50">Log out</Link>
-        // </div>
     )
 }
